fbe8b095ece25212fb29d8dd1b177b239b031991
james-tharit <tharit.rt@gmail.com>
2023-01-29 15:44:33 +0700

    [FEATURE] Spin-up Mongo DB & connect go with Mongo

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	api "gin_ws/api/album"
+	db "gin_ws/db"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,10 +11,25 @@ import (
 func main() {
 	router := gin.Default()
 	router.SetTrustedProxies([]string{"127.0.0.1"})
+	// Get Client, Context, CancelFunc and
+	// err from connect method.
+	client, ctx, cancel, err := db.Connect("mongodb://mongo:27017")
+	if err != nil {
+		panic(err)
+	}
 
+	if err != nil {
+		print("connect error", err)
+	}
+	// Release resource when the main
+	// function is returned.
+	defer db.Close(client, ctx, cancel)
+
+	// Ping mongoDB with Ping method
+	db.Ping(client, ctx)
 	router.GET("/albums", api.GetAlbums)
 	router.POST("/albums", api.PostAlbums)
-	router.GET("/", func(c *gin.Context) {
+	router.GET("/clientIP", func(c *gin.Context) {
 		// If the client is 192.168.1.2, use the X-Forwarded-For
 		// header to deduce the original client IP from the trust-
 		// worthy parts of that header.
